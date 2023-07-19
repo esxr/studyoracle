43fb3ed10affcb0f0dd6d606ab171ade164be069
Eschxr <dhoolia.pranav@gmail.com>
2023-07-19 17:46:29 +1000

    integrate celery worker and db

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- image.tf
+++ image.tf
@@ -7,7 +7,7 @@ resource "docker_image" "studyoracle_server" {
   name = "${aws_ecr_repository.studyoracle_server.repository_url}:latest"
   build {
     context    = "."
-    dockerfile = "Dockerfile"
+    dockerfile = "Dockerfile.server"
   }
 }
 
@@ -18,3 +18,27 @@ resource "docker_registry_image" "studyoracle_server" {
 output "ecr_repository_url_server" {
   value = aws_ecr_repository.studyoracle_server.repository_url
 }
+
+#------------------------------------------------------------
+# Build and push the worker image to ECR (with different build arguements)
+resource "aws_ecr_repository" "studyoracle_worker" {
+  name = "studyoracle_worker"
+}
+
+resource "docker_image" "studyoracle_worker" {
+  name = "${aws_ecr_repository.studyoracle_worker.repository_url}:latest"
+  build {
+    context    = "."
+    dockerfile = "Dockerfile.worker"
+  }
+}
+
+resource "docker_registry_image" "studyoracle_worker" {
+  name = docker_image.studyoracle_worker.name
+}
+
+output "ecr_repository_url_worker" {
+  value = aws_ecr_repository.studyoracle_worker.repository_url
+}
+
+#------------------------------------------------------------

e4e82b0a09f75804983b7ed3ab3ee8ba268ea987
Eschxr <dhoolia.pranav@gmail.com>
2023-07-22 17:19:59 +1000

    MAJOR UPDATE: Make server async and add database
    - Offload file upload and message handlers to a celery worker
    - (PENDING) Celery worker hot-reload
    - (PENDING) checking status of tasks and actual endpoints to display results

--- server/tasks/studyoracle.py
+++ server/tasks/studyoracle.py
@@ -1,114 +1,43 @@
 import os
-
 import time
 import datetime
 from celery import Celery
+from langchain.text_splitter import TextSplitter, RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain import OpenAI, VectorDBQA
+import io
+
+from server.utils import convert_pdf_to_documents, init_vector_store, retrieve_info
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 celery.conf.task_default_queue = os.environ.get("CELERY_DEFAULT_QUEUE", "studyoracle")
 
-import os
-import json
-import subprocess
-import tempfile
+# Global variable to store the vector store
+vectorstore = None
 
 @celery.task(name="doc")
-def create_ticket(ticket_data):
-    # Given the json
-    sample_input_data = {
-        "id": "5e976680-8b7a-4db6-a3b9-cd5c8161f305",
-        "name": "Evan Hughes",
-        "email": "example@uq.edu.au",
-        "concert": {
-            "id": "12345678-1234-1234-1234-123456789012",
-            "name": "Phantom of the Opera",
-            "date": "2023-06-07",
-            "venue": "Sydney Opera House"
-        }
-    }
-
-    # Convert the json to a string
-    input_json = json.dumps(ticket_data)
+def add_doc(file_content):
     try:
-        # Create a temporary file to store the input JSON
-        with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
-            temp_file.write(input_json)
-            temp_file_path = temp_file.name
+        file_stream = io.BytesIO(file_content)
+        documents = convert_pdf_to_documents(file_stream)
 
-        # Construct the command with the temporary file path as an argument
-        command = [
-            "./hamilton",
-            "generate",
-            "ticket",
-            "--input",
-            temp_file_path,
-            "--output",
-            "output"
-        ]
+        global vectorstore
+        vectorstore = init_vector_store(documents)
 
-        # Run the command
-        proc = subprocess.run(command, check=True, capture_output=True, text=True)
-        with open("output.svg", "r") as svg_file:
-            svg_contents = svg_file.read()
-
-        # Return the SVG contents
-        return svg_contents
-
-    except subprocess.CalledProcessError as e:
-        print("Error executing the command:", e)
-
-    finally:
-        # Clean up the temporary file
-        if temp_file_path:
-            os.remove(temp_file_path)
-
+        return "File uploaded successfully"
+    except Exception as e:
+        # print the error
+        print("Error uploading file: ", e)
+        
+    
 @celery.task(name="ask")
-def generate_seating(seating_plan_data):
-    sample_data = {
-        "id": "12345678-1234-1234-1234-123456789012",
-        "name": "Phantom of the Opera",
-        "date": "2023-06-07",
-        "venue": "Sydney Opera House",
-        "seats": {
-            "max": 5738,
-            "purchased": 2340
-        }
-    }
+def handle_message(query):
+    if vectorstore is None:
+        return "No documents have been uploaded yet", 400
 
-    # Convert the json to a string
-    input_json = json.dumps(seating_plan_data)
-    try:
-        # Create a temporary file to store the input JSON
-        with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
-            temp_file.write(input_json)
-            temp_file_path = temp_file.name
-
-        # Construct the command with the temporary file path as an argument
-        command = [
-            "./hamilton",
-            "generate",
-            "seating",
-            "--input",
-            temp_file_path,
-            "--output",
-            "output"
-        ]
-
-        # Run the command
-        proc = subprocess.run(command, check=True, capture_output=True, text=True)
-        with open("output.svg", "r") as svg_file:
-            svg_contents = svg_file.read()
-
-        # Return the SVG contents
-        return svg_contents
-
-    except subprocess.CalledProcessError as e:
-        print("Error executing the command:", e)
-
-    finally:
-        # Clean up the temporary file
-        if temp_file_path:
-            os.remove(temp_file_path)
-
+    # get the answer
+    answer = retrieve_info(query, vectorstore)
+    return answer

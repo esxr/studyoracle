fc5707674b008ae97a2afe7d0d5fae4612e8b9e1
Eschxr <dhoolia.pranav@gmail.com>
2023-07-26 16:37:50 +1000

    HOTFIX for frontend: adpat to API routes
    Depreciated: NextJS to be swapped out in the next few commits

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
-import { Layout, theme } from 'antd';
+import { Button, Layout, theme } from 'antd';
 import TopBar from '../components/TopBar';
+import { useAuth0 } from '@auth0/auth0-react';
 // import GoogleAuth from '@/components/GoogleAuth';
 
 const { Header, Sider, Content } = Layout;
@@ -9,6 +10,20 @@ const { useToken } = theme;
 
 export default function Login() {
   const { token } = useToken();
+
+  const {
+    user,
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+
+  const logoutWithRedirect = () =>
+    logout({
+      logoutParams: {
+        returnTo: (typeof window !== "undefined") ? window.location.origin : null,
+      }
+    });
 
   return (
     <div style={{ height: '100vh' }}>
@@ -27,7 +42,14 @@ export default function Login() {
 
           <Content style={{ backgroundColor: token.colorBgBase }}>
             <div style={{ height: '100%', borderLeft: '1px solid', borderLeftColor: token.colorSecondary, padding: 20, display: 'flex', flexDirection: 'column' }}>
-                {/* <GoogleAuth /> */}
+              <Button
+                id="qsLoginBtn"
+                color="primary"
+                block
+                onClick={() => loginWithRedirect({})}
+              >
+                Log in
+              </Button>
             </div>
           </Content>
         </Layout>

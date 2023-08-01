769baac5c3decc04987c1115fcbbb37fa9f5e445
Eschxr <dhoolia.pranav@gmail.com>
2023-08-01 17:30:14 +1000

    change protocol to HTTPS for testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api/v1": {
-        target: "http://localhost:8080",
+        target: "https://localhost:8080",
         changeOrigin: true,
         secure: false,
       },
